42afd38386c7b446953bb8e1331aa261cf990528
pengxd <pengxd@hua-cloud.com.cn>
2021-05-15 17:09:32 +0800

    项目搭建中

--- gateway-service/src/main/java/io/shadon/cloudservice/GatewayServiceApplication.java
+++ gateway-service/src/main/java/io/shadon/cloudservice/GatewayServiceApplication.java
@@ -2,10 +2,12 @@ package io.shadon.cloudservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @author https://github.com/shadon178
  */
+@EnableDiscoveryClient
 @SpringBootApplication
 public class GatewayServiceApplication {
 
